18a67e05ec58eb472b767a79d135947b460cb35f
Sanket Bhat <sanket.r.bhat.u@gmail.com>
2020-03-17 15:09:06 +0530

    Update ui-tests-on-emulator.sh

--- ui-tests-on-emulator.sh
+++ ui-tests-on-emulator.sh
@@ -7,7 +7,7 @@ START=`date +%s` > /dev/null
 
 echo no | $ANDROID_HOME/tools/bin/avdmanager create avd --force -n test -k "system-images;android-21;google_apis;armeabi-v7a"
 $ANDROID_HOME/tools/bin/avdmanager list avd
-$ANDROID_HOME/tools/emulator64-arm -avd test -no-window -no-boot-anim -no-audio -verbose &
+$ANDROID_HOME/tools/emulator -avd test -no-window -no-boot-anim -no-audio -verbose &
 wait-for-emulator
 unlock-emulator-screen
 
