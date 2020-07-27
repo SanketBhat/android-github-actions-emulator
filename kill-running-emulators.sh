53e1722775c5ffa58476c2827da488ae08f2d0d7
Sanket Bhat <sanket.r.bhat.u@gmail.com>
2020-07-27 12:45:43 +0530

    Update kill-running-emulators.sh

--- kill-running-emulators.sh
+++ kill-running-emulators.sh
@@ -3,4 +3,4 @@
 set -eu
 
 # get rid of running emulators
-$ANDROID_HOME/platform-tools/adb devices | grep emulator | cut -f1 | while read line; do $ANDROID_HOME/platform-tools/adb -s $line emu kill; done
+adb devices | grep emulator | cut -f1 | while read line; do adb -s $line emu kill; done
