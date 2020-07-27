75e8d0eaeb05b909451ab755c4dd9c2d1d8fdc33
Sanket Bhat <sanket.r.bhat.u@gmail.com>
2020-07-27 12:44:39 +0530

    Update ui-tests-on-emulator.sh

--- ui-tests-on-emulator.sh
+++ ui-tests-on-emulator.sh
@@ -3,23 +3,7 @@
 set -eu
 
 # start android emulator
-START=`date +%s` > /dev/null
-
-echo no | $ANDROID_HOME/tools/bin/avdmanager create avd -f -n test -k "system-images;android-25;google_apis;armeabi-v7a"
-chmod +x gradlew
-./gradlew buildDebug
-$ANDROID_HOME/tools/bin/avdmanager list avd
-$ANDROID_HOME/emulator/emulator -avd test -no-window -no-boot-anim -no-audio -partition-size 1024 -verbose &
-wait-for-emulator
+starter
 unlock-emulator-screen
-
-DURATION=$(( `date +%s` - START )) > /dev/null
-echo "Android Emulator started after $DURATION seconds."
-
-# emulator isn't ready yet, wait 1 min more
-# prevents APK installation error
-sleep 180
-
 run-ui-tests
-
 kill-running-emulators
